1084ecdc44fa09889c3670679dc93d961a6f9929
Nicholas Fazzolari <pdxpunk85@gmail.com>
2019-06-13 22:37:45 -0700

    Basic TODO list functionality implemented. Needs a lot of form validation and state managment.

--- todo_vanilla/todo.js
+++ todo_vanilla/todo.js
@@ -1,14 +1,60 @@
 // Author: Nicholas Fazzolari
 
 let listNameInput;
-let listRoot = document.getElementById('listRoot');
-let listTitleEl = document.createElement('h1');
-listTitleEl.id = "listTitle"
+let listItemInput;
+let listTitleEl;
+let newListEl;
+let listItemEl;
+let listItemCount = 0;
+let listRoot = document.getElementById('listRoot'); // Entry point for DOM manipulation
 
-function createListTitle() {
+// Gets the user input value of the form element for the list creation
+function getListName() {
     listNameInput = document.getElementById('listTitle').value;
+}
+
+// Inserts a heading element and fills it with the form value
+function createList() {
+    listTitleEl = document.createElement('h1');
+    listTitleEl.id = "listTitle";
+    listTitleEl.innerText = listNameInput;
+
+    newListEl = document.createElement('ul');
+    newListEl.id = "todoList";
+
     listRoot.append(listTitleEl);
+    listRoot.append(newListEl);
+}
+
+// Wrapped up for use with the button
+function eventCreateList() {
+    getListName();
+    createList();
+}
+
+// Gets user input value of the list item
+function getListItemName() {
+    listItemInput = document.getElementById('listItem').value;
+}
+
+// Inserts the list item into the DOM
+function createListItem() {
+    listItemEl = document.createElement('li');
+    listItemCount++;
+    newListEl.id = listItemCount.toString();
+    listItemEl.innerText = listItemInput;
+
+    newListEl.append(listItemEl);
+}
+
+// Wrapped up for use with the add listen item button
+function eventCreateListItem() {
+    getListItemName();
+    createListItem();
 }
 
 const submitListName = document.getElementById('submitListName');
-submitListName.addEventListener('click', createListTitle, false);
+submitListName.addEventListener('click', eventCreateList, false);
+
+const submitListItem = document.getElementById('submitListItem');
+submitListItem.addEventListener('click', eventCreateListItem, false);
