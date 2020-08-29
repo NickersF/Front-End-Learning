2c9132e8af1e1df2deb080f4c6fe040f95565ab0
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2020-08-28 18:24:38 -0700

    Need to clean up and Style

--- addition_evaluator/main.js
+++ addition_evaluator/main.js
@@ -10,17 +10,35 @@ The user must input and answer and the program will determine if it's correct.
 let a;                  // stores rng for expression
 let b;                  // stores rng for expression
 let strExpr;            // stores the string which will be attached to a DOM node for rendering
-let userAnswer = 2;         // stores the user input value to be used for comparison against the evaluated a & b.
+let userAnswer;     // stores the user input value to be used for comparison against the evaluated a & b.
+// DOM Elements
+let expressionSpanEl = document.getElementById('expressionSpan');
+let userAnswerInput = document.getElementById('answerInput');
+let userAnswerEl = document.getElementById('userAnswer');
+// Button Elements
+let generateExpressionButton = document.getElementById('generateExpressionBtn');
+let submiteAnswerButton = document.getElementById('submitAnswerBtn');
+
 
 // Generate numeric values for the expression
 function createExpressionValues() {
     a = Math.round(Math.random() * 10);
     b = Math.round(Math.random() * 10);
+    userAnswerInput.value = "";
+    userAnswer = "";
+    userAnswerEl.innerHTML = "";
 }
 
 // Convert numbers into a string for page rendering
 function generateExpressionDOMString(a, b) {
-    return a.toString() + " + " + b.toString() + " = ?";
+    expressionSpanEl.innerHTML = a.toString() + " + " + b.toString() + " = ?";
+}
+
+// Get user input and store value
+function getUserInputValue() {
+    userAnswer = userAnswerInput.value;
+    userAnswerEl.innerHTML = userAnswer;
+    
 }
 
 // Evaluate user input value to result of expression
@@ -33,15 +51,16 @@ function evaluateExpression(a, b, userAnswer) {
     }
 }
 
+// DOM Events
+generateExpressionButton.addEventListener('click', createExpressionValues);
+generateExpressionButton.addEventListener('click', function() { generateExpressionDOMString(a, b); });
+userAnswerInput.addEventListener('mouseleave', getUserInputValue);
+submiteAnswerButton.addEventListener('click', function() { evaluateExpression(a, b, parseInt(userAnswer)); })
+
+
 // Function calls
-createExpressionValues(a, b);
-strExpr = generateExpressionDOMString(a, b);
+
+getUserInputValue();
 
 // Evaluation test ( hard coded )
 evaluateExpression(1, 1, userAnswer);
-
-// Logging
-console.log(strExpr);
-console.log(typeof (a));
-console.log(typeof (b));
-console.log(typeof (strExpr));
