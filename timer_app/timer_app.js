4ae4c6d9c59c029f679b41b77baf0da5b2d346f4
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2020-10-10 13:03:02 -0700

    Added dependencies and assets.

--- timer_app/timer_app.js
+++ timer_app/timer_app.js
@@ -4,7 +4,6 @@ Author: Nicholas Fazzolari
 JavaScript implementation for a basic HTML/CSS/JS Timer script.
  */
 
-const timeValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let arrPos = 0;
 
 $(document).ready(function(){
@@ -14,8 +13,7 @@ $(document).ready(function(){
             arrPos = 9;
         }
 
-        $('#valueDisplay').text(timeValues[arrPos]);
-        $('#arrPosDisplay').text(arrPos);
+        $('#valueDisplay').text(arrPos);
         arrPos++;
     });
 
@@ -25,8 +23,7 @@ $(document).ready(function(){
             arrPos = 0;
         }
 
-        $('#valueDisplay').text(timeValues[arrPos]);
-        $('#arrPosDisplay').text(arrPos);
+        $('#valueDisplay').text(arrPos);
         arrPos--;
     });
 });
