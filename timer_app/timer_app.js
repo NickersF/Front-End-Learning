36d803184afe6dabbe4552c58c6f9b9a22f8011f
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2020-10-12 01:11:29 -0700

    Connected events to time value selectors.

--- timer_app/timer_app.js
+++ timer_app/timer_app.js
@@ -4,36 +4,155 @@ Author: Nicholas Fazzolari
 JavaScript implementation for a basic HTML/CSS/JS Timer script.
  */
 
-let posVal = 0;
+let hoursPosVal = 0;
+let minutesPosVal = 0;
+let secondsTensPosVal = 0;
+let secondsOnesPosVal = 0;
 
 $(document).ready(function(){
 
+    // Hours up button events
+    // ***********************
     $('#hoursUpBtn').mousedown(function() {
         $('#hoursUpBtn').attr('src', './set_up_glow.svg');
+
+        if (hoursPosVal >= 5) {
+            hoursPosVal = 5;
+        }
+        hoursPosVal++;
+
+        $('#hoursSetDisplay').text(hoursPosVal);
     });
 
     $('#hoursUpBtn').mouseup(function() {
         $('#hoursUpBtn').attr('src', './set_up.svg');
     });
 
-    $('#upBtn').click(function() {
-
-        if (posVal >= 8) {
-            posVal = 8;
+    // Hours down button events
+    // *************************
+    $('#hoursDnBtn').mousedown(function() {
+        $('#hoursDnBtn').attr('src', './set_down_glow.svg');
+        
+        if (hoursPosVal <= 0) {
+            hoursPosVal = 1;
         }
+        hoursPosVal--;
 
-        posVal++;
-        $('#valueDisplay').text(posVal);
+        $('#hoursSetDisplay').text(hoursPosVal);
     });
 
-    $('#dnBtn').click(function() {
-
-        if (posVal <= 0) {
-            posVal = 1;
-        }
-
-        posVal--;
-        $('#valueDisplay').text(posVal);
+    $('#hoursDnBtn').mouseup(function() {
+        $('#hoursDnBtn').attr('src', './set_down.svg');
     });
 
+    // Minutes up button events
+    // *************************
+    $('#minutesUpBtn').mousedown(function() {
+        $('#minutesUpBtn').attr('src', './set_up_glow.svg');
+
+        if (minutesPosVal >= 8) {
+            minutesPosVal = 8;
+        }
+        minutesPosVal++;
+
+        $('#minutesSetDisplay').text(minutesPosVal);
+    });
+
+    $('#minutesUpBtn').mouseup(function() {
+        $('#minutesUpBtn').attr('src', './set_up.svg');
+    });
+
+    // Minutes down button events
+    // ***************************
+    $('#minutesDnBtn').mousedown(function() {
+        $('#minutesDnBtn').attr('src', './set_down_glow.svg');
+        
+        if (minutesPosVal <= 0) {
+            minutesPosVal = 1;
+        }
+        minutesPosVal--;
+
+        $('#minutesSetDisplay').text(minutesPosVal);
+    });
+
+    $('#minutesDnBtn').mouseup(function() {
+        $('#minutesDnBtn').attr('src', './set_down.svg');
+    });
+
+    // Seconds tens up button events
+    // ******************************
+    $('#tensSecsUpBtn').mousedown(function() {
+        $('#tensSecsUpBtn').attr('src', './set_up_glow.svg');
+
+        if (secondsTensPosVal >= 5) {
+            secondsTensPosVal = 5;
+        }
+        secondsTensPosVal++;
+
+        $('#secondsTensDisplay').text(secondsTensPosVal);
+    });
+
+    $('#tensSecsUpBtn').mouseup(function() {
+        $('#tensSecsUpBtn').attr('src', './set_up.svg');
+    });
+
+    // Seconds tens down button events
+    // ********************************
+    $('#tensSecsDnBtn').mousedown(function() {
+        $('#tensSecsDnBtn').attr('src', './set_down_glow.svg');
+        
+        if (secondsTensPosVal <= 0) {
+            secondsTensPosVal = 1;
+        }
+        secondsTensPosVal--;
+
+        $('#secondsTensDisplay').text(secondsTensPosVal);
+    });
+
+    $('#tensSecsDnBtn').mouseup(function() {
+        $('#tensSecsDnBtn').attr('src', './set_down.svg');
+    });
+
+    // Seconds ones up button events
+    // ******************************
+    $('#onesSecsUpBtn').mousedown(function() {
+        $('#onesSecsUpBtn').attr('src', './set_up_glow.svg');
+
+        if (secondsOnesPosVal >= 8) {
+            secondsOnesPosVal = 8;
+        }
+        secondsOnesPosVal++;
+
+        $('#secondsOnesDisplay').text(secondsOnesPosVal);
+    });
+
+    $('#onesSecsUpBtn').mouseup(function() {
+        $('#onesSecsUpBtn').attr('src', './set_up.svg');
+    });
+
+        // Seconds tens down button events
+    // ********************************
+    $('#onesSecsDnBtn').mousedown(function() {
+        $('#onesSecsDnBtn').attr('src', './set_down_glow.svg');
+        
+        if (secondsOnesPosVal <= 0) {
+            secondsOnesPosVal = 1;
+        }
+        secondsOnesPosVal--;
+
+        $('#secondsOnesDisplay').text(secondsOnesPosVal);
+    });
+
+    $('#onesSecsDnBtn').mouseup(function() {
+        $('#onesSecsDnBtn').attr('src', './set_down.svg');
+    });
+
+    // Clear Button
+    // ************
+    $('#clearButton').click(function() {
+        $('#hoursSetDisplay').text(hoursPosVal = 0);
+        $('#minutesSetDisplay').text(minutesPosVal = 0);
+        $('#secondsTensDisplay').text(secondsTensPosVal = 0);
+        $('#secondsOnesDisplay').text(secondsOnesPosVal = 0);
+    });
 });
