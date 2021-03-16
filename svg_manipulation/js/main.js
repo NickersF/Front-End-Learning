c7c2d6709cedb17d7ceabc1b10092913868e8c4f
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2021-03-15 20:28:13 -0700

    Added SVG manipulation project

---
+++ svg_manipulation/js/main.js
@@ -0,0 +1,12 @@
+window.onload=function() {
+	// Get the Object by ID
+	var a = document.getElementById("svgObject");
+	// Get the SVG document inside the Object tag
+	var svgDoc = a.contentDocument;
+	// Get one of the SVG items by ID;
+	var svgItem = svgDoc.getElementById("Shape_A");
+
+    console.log(svgItem);
+	// Set the colour to something else
+	//svgItem.setAttribute("fill", "lime");
+};
