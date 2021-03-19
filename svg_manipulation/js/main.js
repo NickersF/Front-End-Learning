bbd85fc71335f8c8781cf1bb45c88dd26678fbd2
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2021-03-18 20:16:30 -0700

    Added panel to navs

--- svg_manipulation/js/main.js
+++ svg_manipulation/js/main.js
@@ -1,18 +1,22 @@
-window.onload=function() {
+// window.onload=function() {
 	
-	var svgObject = document.getElementById("svgObject"); 	// Get the Object by ID
-	var svgDocument = svgObject.contentDocument; 			// Get the SVG document inside the Object tag
-	var svgItem = svgDocument.getElementById("Shape_A"); 	// Get one of the SVG items by ID;
+// 	var svgObject = document.getElementById("svgObject"); 	// Get the Object by ID
+// 	var svgDocument = svgObject.contentDocument; 			// Get the SVG document inside the Object tag
+// 	var svgItem = svgDocument.getElementById("Shape_A"); 	// Get one of the SVG items by ID;
 
-	//svgItem.setAttribute("fill", "lime"); 				// Set the colour to something else
-    console.log(svgItem);
+// 	//svgItem.setAttribute("fill", "lime"); 				// Set the colour to something else
+//     console.log(svgItem);
 
-};
+// };
+
+var count = 0;
 
 $(document).ready(function(){
-
-	$("#Shape_A").mouseover(function() {
+	
+	$("#mOver").mouseover(function() {
 		
+		count++;
+		console.log(count);
 	});
-  
+
   }); 
